ca77ecf36df8f57113ceb4c7ac3bc171310806e1
Jordi Domènech <jdomenechbborges@gmail.com>
2020-10-31 23:03:19 +0100

    Replace render function per title by render function per pixel

--- src/gpu/gpu.rs
+++ src/gpu/gpu.rs
@@ -96,8 +96,8 @@ impl GPU {
                 return;
             }
 
-            let bg_tile_map_location = if lcdc.bg_tile_map_display_select() {0x9C00} else { 0x9800 };
-            let bg_data_location = if lcdc.bg_and_window_tile_data_select() {0x8000} else { 0x8800 };
+            let bg_tile_map_start_location : u16 = if lcdc.bg_tile_map_display_select() {0x9C00} else { 0x9800 };
+            let bg_data_start_location : u16 = if lcdc.bg_and_window_tile_data_select() {0x8000} else { 0x8800 };
             let scx = memory.scx();
             let scy = memory.scy();
 
@@ -106,44 +106,54 @@ impl GPU {
 
             let transform = context.transform;
 
-            for y in 0..18 {
-                for x in 0..20 {
-                    let mem_location = bg_tile_map_location + y * BACKGROUND_MAP_TILE_SIZE_X + x;
-                    let bg_offset = bg_data_location + memory.read_8(mem_location) as u16 * TILE_SIZE_BYTES as u16;
+            for screen_y in 0..(PIXEL_HEIGHT as u16) {
+                for screen_x in 0..(PIXEL_WIDTH as u16) {
+                    // Background
+                    let bg_tile_map_location = bg_tile_map_start_location
+                        + (screen_y / PIXELS_PER_TILE) * BACKGROUND_MAP_TILE_SIZE_X
+                        + (screen_x / PIXELS_PER_TILE);
 
-                    for tile_row in 0u16..PIXELS_PER_TILE {
-                        let byte1 = memory.read_8(bg_offset + (tile_row as u16) * 2);
-                        let byte2 = memory.read_8(bg_offset + (tile_row as u16) * 2 + 1);
+                    let bg_data_location = bg_data_start_location
+                        + memory.read_8(bg_tile_map_location) as u16 * TILE_SIZE_BYTES as u16;
 
-                        for bit_pos in (0..8u8).rev() {
-                            let pixel_bit1 = (byte1 >> bit_pos) & 0b1;
-                            let pixel_bit0 = (byte2 >> bit_pos) & 0b1;
+                    let tile_row = screen_y as u16 % 8;
 
-                            let pixel = ((pixel_bit1 << 1) | pixel_bit0) & 0b11;
-                            let pixel = match pixel {
-                                0b11 => bgp >> 6,
-                                0b10 => bgp >> 4,
-                                0b01 => bgp >> 2,
-                                0b00 => bgp >> 0,
-                                _ => panic!("Unrecognised color")
-                            } & 0b11;
+                    let byte1 = memory.read_8(bg_data_location + tile_row * 2);
+                    let byte2 = memory.read_8(bg_data_location + tile_row * 2 + 1);
 
-                            let color = match pixel {
-                                0b00 => Color::WHITE,
-                                0b01 => Color::DARK_GRAY,
-                                0b10 => Color::GRAY,
-                                0b11 => Color::BLACK,
-                                _ => panic!("Unrecognised color")
-                            };
+                    let bit_pos = 7 - (screen_x % 8);
 
-                            let x_pixel = (x as u16) * (PIXELS_PER_TILE as u16) + (7 - bit_pos as u16);
-                            let y_pixel = (y as u16) * (PIXELS_PER_TILE as u16) + (tile_row as u16);
+                    let pixel_bit1 = (byte1 >> bit_pos) & 0b1;
+                    let pixel_bit0 = (byte2 >> bit_pos) & 0b1;
 
-                            let square = rectangle::rectangle_by_corners(x_pixel as f64 * pixel_size.0, y_pixel as f64 * pixel_size.1, (x_pixel + 1) as f64 * pixel_size.0, (y_pixel + 1) as f64 * pixel_size.1);
+                    let pixel = ((pixel_bit1 << 1) | pixel_bit0) & 0b11;
+                    let pixel = match pixel {
+                        0b11 => bgp >> 6,
+                        0b10 => bgp >> 4,
+                        0b01 => bgp >> 2,
+                        0b00 => bgp >> 0,
+                        _ => panic!("Unrecognised color")
+                    } & 0b11;
 
-                            rectangle(color, square, transform, graphics);
-                        }
-                    }
+                    let color = match pixel {
+                        0b00 => Color::WHITE,
+                        0b01 => Color::DARK_GRAY,
+                        0b10 => Color::GRAY,
+                        0b11 => Color::BLACK,
+                        _ => panic!("Unrecognised color")
+                    };
+
+                    let x_pixel = (screen_x as u16) + (7 - bit_pos as u16);
+                    let y_pixel = (screen_y as u16) + (tile_row as u16);
+
+                    let square = rectangle::rectangle_by_corners(
+                        screen_x as f64 * pixel_size.0,
+                        screen_y as f64 * pixel_size.1,
+                        (screen_x + 1) as f64 * pixel_size.0,
+                        (screen_y + 1) as f64 * pixel_size.1
+                    );
+
+                    rectangle(color, square, transform, graphics);
                 }
             }
         });
