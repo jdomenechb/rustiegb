663f1d1b1479affa65414db207452d74f296decb
Jordi Domènech <jdomenechbborges@gmail.com>
2020-11-11 00:15:49 +0100

    Implemented iterator for reading OamEntrys easily

--- src/memory/oam_entry.rs
+++ src/memory/oam_entry.rs
@@ -6,12 +6,12 @@ pub struct OamEntry {
 }
 
 impl OamEntry {
-    pub fn new() -> OamEntry {
+    pub fn from_bytes(position_x: u8, position_y: u8, tile_number: u8, flags: u8) -> OamEntry {
         OamEntry {
-            position_x: 0,
-            position_y: 0,
-            tile_number: 0,
-            flags: 0,
+            position_x,
+            position_y,
+            tile_number,
+            flags,
         }
     }
 }
--- src/memory/oam_memory_sector.rs
+++ src/memory/oam_memory_sector.rs
@@ -1,24 +1,22 @@
+use crate::memory::oam_entry::OamEntry;
+
 pub struct OamMemorySector {
-    data: [u8; 0xA0]
+    data: [u8; 0xA0],
+    count: u16,
 }
 
 impl OamMemorySector {
     pub fn new() -> OamMemorySector {
         return OamMemorySector {
-            data: [0; 0xA0]
+            data: [0; 0xA0],
+            count: 0,
         };
     }
 
-    /**
-     * Reads a 8bit value from memory.
-     */
     pub fn read_8(&self, position: u16) -> u8 {
         return self.data[position as usize];
     }
 
-    /**
-     * Reads a 16bit value from memory. First byte is lower part, second is higher.
-     */
     pub fn read_16(&self, position: u16) -> u16{
         let position = position as usize;
         let mut result: u16 = self.data[position] as u16;
@@ -34,5 +32,31 @@ impl OamMemorySector {
         let position = position as usize;
         self.data[position] = value as u8;
         self.data[position + 1] = (value >> 8) as u8;
+    }
+
+    fn read_oam_entry(&self, position: u16) -> OamEntry {
+        let position = position as usize;
+
+        OamEntry::from_bytes(
+            self.data[position],
+            self.data[position + 1],
+            self.data[position + 2],
+            self.data[position + 3]
+        )
+    }
+}
+
+impl Iterator for OamMemorySector {
+    type Item = OamEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count >= self.data.len() as u16 {
+            return None;
+        }
+
+        let result = Some(self.read_oam_entry(self.count));
+        self.count += 4;
+
+        result
     }
 }
