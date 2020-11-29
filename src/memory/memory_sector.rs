cc98a72e7d224501fa54e4fc7d0bf1ccc2b39e73
Jordi Domenech Bonilla <jdomenechbborges@gmail.com>
2020-11-29 13:41:30 +0100

    Use math functions for memory reading and writing

--- src/memory/memory_sector.rs
+++ src/memory/memory_sector.rs
@@ -1,3 +1,5 @@
+use crate::math::{two_u8_to_u16, u16_to_two_u8};
+
 pub trait ReadMemory {
     fn read_8(&self, position: u16) -> u8;
     fn read_16(&self, position: u16) -> u16;
@@ -35,9 +37,8 @@ impl ReadMemory for MemorySector {
 
     fn read_16(&self, position: u16) -> u16 {
         let position = position as usize;
-        let mut result: u16 = self.data[position] as u16;
-        result += (self.data[position + 1] as u16) << 8;
-        return result;
+
+        two_u8_to_u16(self.data[position + 1], self.data[position])
     }
 }
 
@@ -48,7 +49,10 @@ impl WriteMemory for MemorySector {
 
     fn write_16(&mut self, position: u16, value: u16) {
         let position = position as usize;
-        self.data[position] = value as u8;
-        self.data[position + 1] = (value >> 8) as u8;
+
+        let bytes = u16_to_two_u8(value);
+
+        self.data[position] = bytes.1;
+        self.data[position + 1] = bytes.0;
     }
 }
