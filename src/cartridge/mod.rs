5dd4470570d713d0a281c7399fb939227ce0231e
Jordi Domènech <jdomenechb@protonmail.com>
2021-06-20 14:35:07 +0200

    Fixed bug with window not displayed

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -320,7 +320,7 @@ impl WriteMemory for Cartridge {
 
                 // Select ROM Bank Number - High
                 if position >= 0x3000 && position < 0x4000 {
-                    self.selected_rom_bank = (((value & 0x1) as u16) << 8 | self.selected_rom_bank);
+                    self.selected_rom_bank = ((value & 0x1) as u16) << 8 | self.selected_rom_bank;
                     return;
                 }
 
