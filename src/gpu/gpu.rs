ebc4fda8801d2b47f08e2ce8b31bc55c2278a309
Jordi Domènech <jdomenechb@protonmail.com>
2021-07-04 01:57:02 +0200

    Refactored draw_sprites into draw_sprites_row fully

--- src/gpu/gpu.rs
+++ src/gpu/gpu.rs
@@ -208,7 +208,9 @@ impl GPU {
         };
 
         let mut any_window_rendered = false;
-        let mut screen_row: [Option<DisplayPixel>; GPU::PIXEL_WIDTH as usize] =
+        let mut screen_row_no_priority: [Option<DisplayPixel>; GPU::PIXEL_WIDTH as usize] =
+            [None; GPU::PIXEL_WIDTH as usize];
+        let mut screen_row_with_priority: [Option<DisplayPixel>; GPU::PIXEL_WIDTH as usize] =
             [None; GPU::PIXEL_WIDTH as usize];
 
         if lcdc.obj_sprite_display() {
@@ -218,12 +220,21 @@ impl GPU {
                 sprite_palette0,
                 sprite_palette1,
                 sprite_size,
-                &mut screen_row,
+                &mut screen_row_no_priority,
+            );
+
+            self.draw_sprites_in_row(
+                true,
+                screen_y,
+                sprite_palette0,
+                sprite_palette1,
+                sprite_size,
+                &mut screen_row_with_priority,
             );
         }
 
         for screen_x in 0..(GPU::PIXEL_WIDTH as u16) {
-            let pixel_to_write = screen_row.get(screen_x as usize).unwrap();
+            let mut pixel_to_write = *screen_row_no_priority.get(screen_x as usize).unwrap();
 
             if pixel_to_write.is_some() {
                 canvas.put_pixel(
@@ -235,19 +246,7 @@ impl GPU {
                 continue;
             }
 
-            let mut pixel_to_write: Option<DisplayPixel> = None;
-
-            // Sprites with low priority
-            if lcdc.obj_sprite_display() {
-                pixel_to_write = self.draw_sprites(
-                    true,
-                    screen_x,
-                    screen_y,
-                    sprite_palette0,
-                    sprite_palette1,
-                    sprite_size,
-                );
-            }
+            pixel_to_write = *screen_row_with_priority.get(screen_x as usize).unwrap();
 
             if lcdc.bg_display() {
                 let screen_x_with_offset = ((screen_x as u8).wrapping_add(scx)) as u16;
@@ -372,88 +371,6 @@ impl GPU {
         // cache.insert((tile_address, row), bytes);
 
         bytes
-    }
-
-    fn draw_sprites(
-        &self,
-        priority: bool,
-        screen_x: u16,
-        screen_y: u16,
-        palette0: Byte,
-        palette1: Byte,
-        sprite_size: i16,
-    ) -> Option<DisplayPixel> {
-        const SPRITE_TILES_ADDR_START: u16 = 0x8000;
-
-        let sprites_to_be_drawn = match priority {
-            true => &self.sprites_to_be_drawn_with_priority,
-            false => &self.sprites_to_be_drawn_without_priority,
-        };
-
-        let mut last_drawn: Option<&OamEntry> = None;
-
-        for sprite in sprites_to_be_drawn {
-            let current_pixel_x: i16 =
-                screen_x as i16 + GPU::PIXELS_PER_TILE as i16 - sprite.x() as i16;
-
-            if current_pixel_x < 0 || current_pixel_x >= 8 {
-                continue;
-            }
-
-            let current_pixel_y: i16 =
-                screen_y as i16 + (GPU::PIXELS_PER_TILE * 2) as i16 - sprite.y() as i16;
-
-            let sprite_addr =
-                SPRITE_TILES_ADDR_START + sprite.tile_number() as u16 * GPU::TILE_SIZE_BYTES as u16;
-
-            let row = if sprite.flip_y() {
-                sprite_size - 1 - current_pixel_y
-            } else {
-                current_pixel_y
-            } as Word;
-
-            let tile_row = self.read_tile_row(sprite_addr, row);
-
-            let pixel = self.read_pixel_from_tile(
-                if sprite.flip_x() {
-                    7 - current_pixel_x
-                } else {
-                    current_pixel_x
-                } as Word,
-                tile_row,
-            );
-
-            if pixel == 0 {
-                continue;
-            }
-
-            last_drawn = Some(sprite);
-
-            let palette = if !sprite.palette() {
-                palette0
-            } else {
-                palette1
-            };
-
-            let pixel_color = match pixel {
-                0b11 => palette >> 6,
-                0b10 => palette >> 4,
-                0b01 => palette >> 2,
-                _ => panic!("Unrecognised color"),
-            } & 0b11;
-
-            let color = match pixel_color {
-                0b00 => Color::white(),
-                0b01 => Color::light_grey(),
-                0b10 => Color::dark_grey(),
-                0b11 => Color::black(),
-                _ => panic!("Unrecognised color"),
-            };
-
-            return Some(color.to_rgba());
-        }
-
-        None
     }
 
     fn draw_sprites_in_row(
