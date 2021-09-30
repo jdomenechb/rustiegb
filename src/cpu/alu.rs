147234a37a90bb2289136b20439e37d0d5e584d8
Jordi Domènech <jdomenechb@protonmail.com>
2021-10-01 00:20:04 +0200

    Added test_case library

--- src/cpu/alu.rs
+++ src/cpu/alu.rs
@@ -2,7 +2,6 @@ use super::registers::CpuRegisters;
 use crate::cpu::registers::ByteRegister;
 use crate::{Byte, Word};
 
-#[derive(Debug)]
 pub struct Alu {}
 
 impl Alu {
@@ -130,5 +129,39 @@ impl Alu {
 
     pub fn dec_nn(&self, value: Word) -> Word {
         value.wrapping_sub(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(0, 0, false, 0, false, false, true  ; "0 without carry")]
+    #[test_case(0, 0, true, 1, false, false, false  ; "0 with carry")]
+    #[test_case(4, 5, false, 9, false, false, false  ; "normal without carry")]
+    #[test_case(4, 5, true, 10, false, false, false  ; "normal with carry")]
+    #[test_case(10, 6, false, 16, true, false, false  ; "flag h")]
+    #[test_case(0xFF, 1, false, 0, true, true, true  ; "upper limit: flag c,z,h")]
+    #[test_case(0xFF, 5, false, 4, true, true, false  ; "upper limit: flag c,h but not z")]
+    fn test_add_n(
+        a: Byte,
+        b: Byte,
+        carry: bool,
+        expected: Byte,
+        expected_h: bool,
+        expected_c: bool,
+        expected_z: bool,
+    ) {
+        let mut registers = CpuRegisters::new(false);
+        let alu = Alu {};
+
+        let result = alu.add_n(&mut registers, a, b, carry);
+
+        assert_eq!(result, expected);
+        assert!(!registers.is_flag_n());
+        assert_eq!(registers.is_flag_h(), expected_h);
+        assert_eq!(registers.is_flag_c(), expected_c);
+        assert_eq!(registers.is_flag_z(), expected_z);
     }
 }
