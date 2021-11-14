773d0398d3e289364c06f2e5142341ad24086864
Jordi Domenech Bonilla <jdomenechbborges@gmail.com>
2021-11-14 03:14:27 +0100

    Rewritten IFs into match

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -326,367 +326,106 @@ impl Memory {
     }
 
     pub fn write_byte(&mut self, position: Word, value: Byte) {
-        // ROM
-        if position < 0x8000 {
-            self.cartridge.write_byte(position, value);
-            return;
-        }
-
-        // Video RAM
-        if (0x8000..0xA000).contains(&position) {
-            self.video_ram.write_byte(position - 0x8000, value);
-            return;
-        }
-
-        // 8k switchable RAM bank
-        if (0xA000..0xC000).contains(&position) {
-            self.switchable_ram_bank
-                .write_byte(position - 0xA000, value);
-            return;
-        }
-
-        // Internal RAM 8k
-        if (0xC000..0xE000).contains(&position) {
-            self.internal_ram_8k.write_byte(position - 0xC000, value);
-            return;
-        }
-
-        // Echo of internal RAM
-        if (0xE000..0xFE00).contains(&position) {
-            self.internal_ram_8k.write_byte(position - 0xE000, value);
-            return;
-        }
-
-        if (0xFE00..0xFEA0).contains(&position) {
-            self.oam_ram.write_byte(position - 0xFE00, value);
-            return;
-        }
-
-        if (0xFEA0..0xFF00).contains(&position) {
-            println!("Attempt to write at an unused RAM position {:X}", position);
-            return;
-        }
-
-        // P1
-        if position == 0xFF00 {
-            self.p1.from_byte(value);
-            return;
-        }
-
-        // Serial transfer data
-        if position == 0xFF01 {
-            self.serial_transfer_data = value;
-            return;
-        }
-
-        // Serial transfer data
-        if position == 0xFF02 {
-            self.sio_control = value;
-            return;
-        }
-
-        // DIV register
-        if position == 0xFF04 {
-            self.div = 0;
-            return;
-        }
-
-        // TIMA
-        if position == 0xFF05 {
-            self.tima = value;
-            return;
-        }
-
-        // TMA
-        if position == 0xFF06 {
-            self.tma = value;
-            return;
-        }
-
-        // Timer Control
-        if position == 0xFF07 {
-            self.timer_control = value.into();
-            return;
-        }
-
-        // Interrupt Flag
-        if position == 0xFF0F {
-            self.interrupt_flag = value.into();
-            return;
-        }
-
-        // NR10
-        if position == 0xFF10 {
-            self.nr10 = value;
-            return;
-        }
-
-        // NR11
-        if position == 0xFF11 {
-            self.nr11 = value;
-            return;
-        }
-
-        // NR12
-        if position == 0xFF12 {
-            self.nr12 = value;
-            return;
-        }
-
-        // NR13
-        if position == 0xFF13 {
-            self.nr13 = value;
-            return;
-        }
-
-        // NR14
-        if position == 0xFF14 {
-            if value & 0b10000000 == 0b10000000 {
-                self.audio_1_triggered = true;
+        match position {
+            0..=0x7FFF => self.cartridge.write_byte(position, value),
+            0x8000..=0x9FFF => self.video_ram.write_byte(position - 0x8000, value),
+            0xA000..=0xBFFF => self
+                .switchable_ram_bank
+                .write_byte(position - 0xA000, value),
+            0xC000..=0xDFFF => self.internal_ram_8k.write_byte(position - 0xC000, value),
+            0xE000..=0xFDFF => self.internal_ram_8k.write_byte(position - 0xE000, value),
+            0xFE00..=0xFE9F => self.oam_ram.write_byte(position - 0xFE00, value),
+            0xFEA0..=0xFEFF => {
+                println!("Attempt to write at an unused RAM position {:X}", position)
             }
+            0xFF00 => self.p1.from_byte(value),
+            0xFF01 => self.serial_transfer_data = value,
+            0xFF02 => self.sio_control = value,
+            0xFF04 => self.div = 0,
+            0xFF05 => self.tima = value,
+            0xFF06 => self.tma = value,
+            0xFF07 => self.timer_control = value.into(),
+            0xFF0F => self.interrupt_flag = value.into(),
+            0xFF10 => self.nr10 = value,
+            0xFF11 => self.nr11 = value,
+            0xFF12 => self.nr12 = value,
+            0xFF13 => self.nr13 = value,
+            0xFF14 => {
+                if value & 0b10000000 == 0b10000000 {
+                    self.audio_1_triggered = true;
+                }
 
-            self.nr14 = value;
-            return;
-        }
-
-        // NR20
-        if position == 0xFF15 {
-            self.nr20 = value;
-            return;
-        }
-
-        // NR21
-        if position == 0xFF16 {
-            self.nr21 = value;
-            return;
-        }
-
-        // NR22
-        if position == 0xFF17 {
-            self.nr22 = value;
-            return;
-        }
-
-        // NR23
-        if position == 0xFF18 {
-            self.nr23 = value;
-            return;
-        }
-
-        // NR24
-        if position == 0xFF19 {
-            if value & 0b10000000 == 0b10000000 {
-                self.audio_2_triggered = true;
+                self.nr14 = value;
             }
+            0xFF15 => self.nr20 = value,
+            0xFF16 => self.nr21 = value,
+            0xFF17 => self.nr22 = value,
+            0xFF18 => self.nr23 = value,
+            0xFF19 => {
+                if value & 0b10000000 == 0b10000000 {
+                    self.audio_2_triggered = true;
+                }
 
-            self.nr24 = value;
-            return;
-        }
-
-        // NR30
-        if position == 0xFF1A {
-            self.nr30 = value;
-            return;
-        }
-
-        // NR31
-        if position == 0xFF1B {
-            self.nr31 = value;
-            return;
-        }
-
-        // NR32
-        if position == 0xFF1C {
-            self.nr32 = value;
-            return;
-        }
-
-        // NR33
-        if position == 0xFF1D {
-            self.nr33 = value;
-            return;
-        }
-
-        // NR34
-        if position == 0xFF1E {
-            if value & 0b10000000 == 0b10000000 {
-                self.audio_3_triggered = true;
+                self.nr24 = value;
             }
+            0xFF1A => self.nr30 = value,
+            0xFF1B => self.nr31 = value,
+            0xFF1C => self.nr32 = value,
+            0xFF1D => self.nr33 = value,
+            0xFF1E => {
+                if value & 0b10000000 == 0b10000000 {
+                    self.audio_3_triggered = true;
+                }
 
-            self.nr34 = value;
-            return;
-        }
-
-        // NR40
-        if position == 0xFF1F {
-            self.nr40 = value;
-            return;
-        }
-
-        // NR41
-        if position == 0xFF20 {
-            self.nr41 = value;
-            return;
-        }
-
-        // NR42
-        if position == 0xFF21 {
-            self.nr42 = value;
-            return;
-        }
-
-        // NR43
-        if position == 0xFF22 {
-            self.nr43 = value;
-            return;
-        }
-
-        // NR44
-        if position == 0xFF23 {
-            if value & 0b10000000 == 0b10000000 {
-                self.audio_4_triggered = true;
+                self.nr34 = value;
             }
+            0xFF1F => self.nr40 = value,
+            0xFF20 => self.nr41 = value,
+            0xFF21 => self.nr42 = value,
+            0xFF22 => self.nr43 = value,
+            0xFF23 => {
+                if value & 0b10000000 == 0b10000000 {
+                    self.audio_4_triggered = true;
+                }
 
-            self.nr44 = value;
-            return;
-        }
-
-        // NR50
-        if position == 0xFF24 {
-            self.nr50 = value;
-            return;
-        }
-
-        // NR51
-        if position == 0xFF25 {
-            self.nr51 = value;
-            return;
-        }
-
-        // NR52
-        if position == 0xFF26 {
-            self.nr52 = value;
-            return;
-        }
-
-        if (0xFF30..0xFF40).contains(&position) {
-            self.wave_pattern_ram.write_byte(position - 0xFF30, value);
-            return;
-        }
-
-        // LCDC
-        if position == 0xFF40 {
-            self.lcdc = value.into();
-            return;
-        }
-
-        // STAT
-        if position == 0xFF41 {
-            self.stat = value.into();
-            return;
-        }
-
-        // SCY
-        if position == 0xFF42 {
-            self.scy = value;
-            return;
-        }
-
-        // SCX
-        if position == 0xFF43 {
-            self.scx = value;
-            return;
-        }
-
-        // LY
-        if position == 0xFF44 {
-            self.ly = value.into();
-            return;
-        }
-
-        // LYC
-        if position == 0xFF45 {
-            self.lyc = value;
-            return;
-        }
-
-        // DMA
-        if position == 0xFF46 {
-            self.dma = value;
-
-            // DMA Transfer
-            let init_address = (self.dma as Word) << 8 & 0xFF00;
-
-            for i in 0..0xA0 {
-                self.oam_ram.write_byte(i, self.read_byte(init_address + i));
+                self.nr44 = value;
             }
+            0xFF24 => self.nr50 = value,
+            0xFF25 => self.nr51 = value,
+            0xFF26 => self.nr52 = value,
+            0xFF30..=0xFF3F => self.wave_pattern_ram.write_byte(position - 0xFF30, value),
+            0xFF40 => self.lcdc = value.into(),
+            0xFF41 => self.stat = value.into(),
+            0xFF42 => self.scy = value,
+            0xFF43 => self.scx = value,
+            0xFF44 => self.ly = value.into(),
+            0xFF45 => self.lyc = value,
+            0xFF46 => {
+                self.dma = value;
 
-            self.dma = 0;
+                // DMA Transfer
+                let init_address = (self.dma as Word) << 8 & 0xFF00;
 
-            return;
-        }
+                for i in 0..0xA0 {
+                    self.oam_ram.write_byte(i, self.read_byte(init_address + i));
+                }
 
-        // BGP
-        if position == 0xFF47 {
-            self.bgp = value;
-            return;
-        }
-
-        // OBP1
-        if position == 0xFF48 {
-            self.obp1 = value;
-            return;
-        }
-
-        // OBP2
-        if position == 0xFF49 {
-            self.obp2 = value;
-            return;
-        }
-
-        // Window Y
-        if position == 0xFF4A {
-            self.wy = value;
-            return;
-        }
-
-        // Window X
-        if position == 0xFF4B {
-            self.wx = value;
-            return;
-        }
-
-        // Empty but unusable for I/O
-        if position == 0xFF4C {
-            println!("Attempt to write at an unused RAM position {:X}", position);
-            return;
-        }
-
-        // KEY 1
-        if position == 0xFF4D {
-            self.key1 = value;
-            return;
-        }
-
-        // Empty but unusable for I/O
-        if (0xFF4E..0xFF80).contains(&position) {
-            println!("Attempt to write at an unused RAM position {:X}", position);
-            return;
-        }
-
-        // Internal RAM
-        if (0xFF80..0xFFFF).contains(&position) {
-            self.internal_ram.write_byte(position - 0xFF80, value);
-            return;
-        }
-
-        // Interrupt enable
-        if position == 0xFFFF {
-            self.interrupt_enable = value.into();
-            return;
-        }
-
-        panic!("ERROR: Memory address {:X} not writable", position);
+                self.dma = 0;
+            }
+            0xFF47 => self.bgp = value,
+            0xFF48 => self.obp1 = value,
+            0xFF49 => self.obp2 = value,
+            0xFF4A => self.wy = value,
+            0xFF4B => self.wx = value,
+            0xFF4C => println!("Attempt to write at an unused RAM position {:X}", position),
+            0xFF4D => self.key1 = value,
+            0xFF4E..=0xFF7F => {
+                println!("Attempt to write at an unused RAM position {:X}", position)
+            }
+            0xFF80..=0xFFFE => self.internal_ram.write_byte(position - 0xFF80, value),
+            0xFFFF => self.interrupt_enable = value.into(),
+            _ => panic!("ERROR: Memory address {:X} not writable", position),
+        };
     }
 
     pub fn write_word(&mut self, position: Word, value: Word) {
